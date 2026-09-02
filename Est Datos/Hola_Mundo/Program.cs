27f709961d9081b3fbb7d75c9ae479dc98c3ef97
michel <michellelopez.a1508@gmail.com>
2026-09-02 10:05:45 -0700

    Delete Est Datos directory

--- Est Datos/Hola_Mundo/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Hola Mundo :3");
-    }
-}
--- Est Datos/hola.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hola Mundo :3";
-    return 0;
-}   
